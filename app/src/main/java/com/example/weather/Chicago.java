7ad24f184e1a62e3c0796b7d3d911ee103e50608
FranzKnight <finbavan@umich.edi>
2020-04-12 01:59:10 -0400

    Saturday Final Working Project
    For all intents and purposes, the project officially fits all the requirements professor has laid out.
    
    All left to do is make it look nice!

--- app/src/main/java/com/example/weather/Chicago.java
+++ app/src/main/java/com/example/weather/Chicago.java
@@ -4,138 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
+import android.os.Bundle;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+
 
 public class Chicago extends AppCompatActivity {
 
-
     Button home;
-
-
-    TextView idTest;
-    TextView mainWeatherTest;
-    TextView descriptionTest;
-
-    TextView jTesting;
-    TextView tempTest;
-
-
-    String URL = "http://api.openweathermap.org/data/2.5/forecast?q=chicago&appid=5225ba78215711c14d97b36ee6d49633";
-    String dataResults = " ";
-    String tempResults = " ";
-
-
-    String actualTemp;
-    String feelsLikeTemp;
-    String minTemp;
-    String maxTemp;
-
-
-
-    //IMPORT WEATHER INFO FROM API
-    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chicago);
 
-        //Initializing all the buttons and views
 
-        //Buttons
         home = findViewById(R.id.home);
-
-        //Text Views
-        idTest = findViewById(R.id.idTest);
-        mainWeatherTest = findViewById(R.id.mainWeatherTest);
-        descriptionTest = findViewById(R.id.descriptionTest);
-        jTesting = findViewById(R.id.jTesting);
-        tempTest = findViewById(R.id.tempTest);
-
-
-        //End initializing views
-
-
-        //instantiate the request queue
-        requestQueue = Volley.newRequestQueue(this);
-
-        //create object request
-
-
-
-        //BAUGH CODE
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
-                "https://api.openweathermap.org/data/2.5/forecast?q=chicago&appid=5225ba78215711c14d97b36ee6d49633", null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-
-                            JSONArray theList = response.getJSONArray("list");
-                            String TESTJSON = theList.toString();
-
-                            JSONObject firstElement = theList.getJSONObject(0);
-
-                            JSONArray weatherArray = firstElement.getJSONArray("weather");
-
-                            JSONObject theWeather = weatherArray.getJSONObject(0);
-
-                            String TESTJSON2 = weatherArray.toString();
-
-                            tempTest.setText(firstElement.toString());
-
-                            jTesting.setText(TESTJSON2);
-
-                        } catch (JSONException ex) {
-                            Log.e("JSON Error", ex.getMessage());
-                        }
-
-
-                    }
-                },
-
-
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                }
-        );
-
-        requestQueue.add(jsonObjectRequest);
-
-//END OF JSON OBJECT REQUEST
-
-        //jTesting.setText(dataResults);
-
-
-
-
-        //create object request
-
-
-        //PARSE THROUGH INFO
-
-
-        //EXPORT INFO TO SCREEN
-
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -146,6 +32,7 @@ public class Chicago extends AppCompatActivity {
 
             }
         });
+
 
     }
 }
--- app/src/main/java/com/example/weather/detroit.java
+++ app/src/main/java/com/example/weather/detroit.java
@@ -32,26 +32,40 @@ public class detroit extends AppCompatActivity {
     Button home;
 
 
-    TextView idTest;
-    TextView mainWeatherTest;
-    TextView descriptionTest;
+    TextView currentTemp;
+    TextView currentDescription;
 
-    TextView jTesting;
-    TextView tempTest;
-
-
-    String URL = "http://api.openweathermap.org/data/2.5/forecast?q=detroit&appid=5225ba78215711c14d97b36ee6d49633";
-    String dataResults = " ";
-    String tempResults = " ";
+    TextView dayOne;
+    TextView dayTwo;
+    TextView dayThree;
+    TextView dayFour;
+    TextView dayFive;
 
 
-    String actualTemp;
+
+    String URL = "https://api.openweathermap.org/data/2.5/forecast?q=detroit&units=imperial&appid=5225ba78215711c14d97b36ee6d49633";
+
+
+
+
+
+
+
+
+    String currentTemperature;
+    String currentDescriptionText;
     String clouds;
 
 
+
     String feelsLikeTemp;
+
     String minTemp;
     String maxTemp;
+    String dayDescription;
+    String dateInfo;
+
+    String fiveDayForecast;
 
 
 
@@ -68,12 +82,17 @@ public class detroit extends AppCompatActivity {
             //Buttons
         home = findViewById(R.id.home);
 
+
             //Text Views
-        idTest = findViewById(R.id.idTest);
-        mainWeatherTest = findViewById(R.id.mainWeatherTest);
-        descriptionTest = findViewById(R.id.descriptionTest);
-        jTesting = findViewById(R.id.jTesting);
-        tempTest = findViewById(R.id.tempTest);
+
+        currentTemp = findViewById(R.id.currentTemp);
+        currentDescription = findViewById(R.id.currentDescription);
+
+        dayOne = findViewById(R.id.dayOne);
+        dayTwo = findViewById(R.id.dayTwo);
+        dayThree = findViewById(R.id.dayThree);
+        dayFour = findViewById(R.id.dayFour);
+        dayFive = findViewById(R.id.dayFive);
 
 
         //End initializing views
@@ -86,48 +105,116 @@ public class detroit extends AppCompatActivity {
 
 
 
+
         //BAUGH CODE
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
-                "https://api.openweathermap.org/data/2.5/forecast?q=detroit&units=imperial&appid=5225ba78215711c14d97b36ee6d49633", null,
+                URL, null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
 
 
+                        //Returns full JSON List
                             JSONArray theList = response.getJSONArray("list");
 
-                            String TESTJSON = theList.toString();
-
+                        //Parse to first index of "LIST" which shows first time stamp of data
                             JSONObject firstElement = theList.getJSONObject(0);
 
-                            JSONObject mainElement = firstElement.getJSONObject("main");
+                            //Parse to second level of 'main' with temp, feels like, min, max, etc
+                                JSONObject mainElement = firstElement.getJSONObject("main");
 
-                            actualTemp = mainElement.getString("temp");
+                                    //Parse to lower level to get string based on the key string such as "temp", or "temp_min" etc
+                                     currentTemperature = mainElement.getString("temp");
 
-                            JSONObject secondElement = firstElement.getJSONObject("clouds");
-
-                            clouds = secondElement.getString("all");
-
-
-
+                                     //Set textview based on string
+                                     currentTemp.setText(currentTemperature);
 
 
-
-
-
-
+                         //Parse through WEATHER array in the List
                             JSONArray weatherArray = firstElement.getJSONArray("weather");
+
+                            //Parse to the first index of the array
+                            JSONObject currentWeather = weatherArray.getJSONObject(0);
+
+                                    //Parse to the string with the key of "description", and also can access id, main, and icon
+                                    currentDescriptionText = currentWeather.getString("description");
+                                    currentDescription.setText(currentDescriptionText);
+
+
+
+                                    //LOOP to populate 5 day forecast
+                                    for (int index = 0; index < 5; index++)
+                                    {
+                                        //Multiplying index by 8 to increment by each day
+                                        JSONObject loopElement = theList.getJSONObject(index * 8);
+                                        JSONObject loopElementLower = loopElement.getJSONObject("main");
+
+                                        minTemp = loopElementLower.getString("temp_min");
+                                        maxTemp = loopElementLower.getString("temp_max");
+
+                                        JSONArray loopArray = firstElement.getJSONArray("weather");
+                                        JSONObject loopWeather = loopArray.getJSONObject(0);
+
+                                        dayDescription = loopWeather.getString("description");
+
+                                       // JSONObject dateElement = loopElement.getJSONObject("sys");
+                                        dateInfo = loopElement.getString("dt_txt");
+
+                                        fiveDayForecast = "Date: " + dateInfo + "MIN:" + minTemp + "| " + "MAX:" + maxTemp + "DESC: " + dayDescription;
+
+                                        if (index == 0)
+                                        {
+                                            dayOne.setText(fiveDayForecast);
+                                        }
+                                        else if (index == 1)
+                                        {
+                                            dayTwo.setText(fiveDayForecast);
+                                        }
+                                        else if (index == 2)
+                                        {
+                                            dayThree.setText(fiveDayForecast);
+                                        }
+                                        else if (index == 3)
+                                        {
+                                            dayFour.setText(fiveDayForecast);
+                                        }
+                                        else if (index == 4)
+                                        {
+                                            dayFive.setText(fiveDayForecast);
+                                        }
+
+                                    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
                             JSONObject theWeather = weatherArray.getJSONObject(0);
 
-                            String TESTJSON2 = weatherArray.toString();
 
 
-                            tempTest.setText(clouds);
 
-                            jTesting.setText(actualTemp);
+
+
+
+
+
+
+
 
 
 
